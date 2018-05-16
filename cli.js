754bb37355e483bd8d3e2d16a80c56a9b3531e3a
mkor <31717889+mkora@users.noreply.github.com>
2018-05-15 21:42:38 -0400

    Add prompt questions for get action

--- cli.js
+++ cli.js
@@ -8,6 +8,8 @@ const {
 } = require('./utils/chalk-init');
 const inquirer = require('inquirer');
 
+const testFilename = require('./utils/filename-regex');
+
 /**
  * Init CLI object
  *  + add non-flag arguments
@@ -43,6 +45,44 @@ const cli = meow(`
     },
   },
 });
+
+/**
+ * Init promting question object
+ * for mo options see Inquirer documentation
+ */
+const getQuestions = [
+  {
+    type: 'input',
+    name: 'identifier',
+    message: 'Please, enter identifier',
+    validate: (value) => {
+      if (testFilename(value)) {
+        return true;
+      }
+      return 'Please enter a valid identifier name';
+    },
+  },
+  {
+    type: 'input',
+    name: 'size',
+    message: 'Please, provide a size of a pic (in px)',
+    validate: (value) => {
+      const parsed = parseFloat(value);
+      return !isNaN(parseFloat(parsed)) || 'Please enter a number';
+    },
+    filter: Number,
+  },
+  {
+    type: 'input',
+    name: 'radius',
+    message: 'Please, provide a border radius (%)',
+    validate: (value) => {
+      const parsed = parseFloat(value);
+      return !((isNaN(parsed) || parsed < 0 || parsed > 100)) || 'Please enter a valid number';
+    },
+    filter: Number,
+  },
+];
 
 /**
  * Reacts on cli input and options
---
+++ utils/filename-regex.js
@@ -0,0 +1,1 @@
+module.exports = filename => filename.match(/([^\\\/]+)$/);
