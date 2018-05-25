cb895b26bb9a46b20a23f6caed97c7fc1d041d64
mkor <31717889+mkora@users.noreply.github.com>
2018-05-25 18:50:09 -0400

    Update actions

--- actions/index.js
+++ actions/index.js
@@ -6,35 +6,40 @@ const {
   warn, // eslint-disable-line no-unused-vars
   success, // eslint-disable-line no-unused-vars
   debug, // eslint-disable-line no-unused-vars
-  log,
+  log, // eslint-disable-line no-unused-vars
 } = require('../utils/chalk-init');
 const getQuestions = require('../questions/get');
+const makeQuestions = require('../questions/make');
 const config = require('../config');
+
+const getAvatar = (url, filename) => {
+  const folder = config.DEFAULT_FOLDER;
+  debug(`Let's make an API call to ${url}`);
+
+  axios({
+    method: 'get',
+    url,
+    responseType: 'stream',
+  })
+    .then((res) => {
+      res.data.pipe(fs.createWriteStream(`${folder}${filename}`));
+      success(`Image was saved to ${folder}${filename}`);
+    })
+    .catch((err) => {
+      error(err);
+    });
+  log(`You can also access avatar at ${url}`);
+};
 
 const getAction = (opts) => {
   debug(opts);
 
   inquirer.prompt(getQuestions).then((answers) => {
     debug(answers);
-
     if (answers.id) {
       const filename = `${answers.id}.png`;
       const url = `${config.API_URL}${answers.size}/${filename}`;
-      const folder = config.DEFAULT_FOLDER;
-      debug(`Let's make an API call to ${url}`);
-
-      axios({
-        method: 'get',
-        url,
-        responseType: 'stream',
-      })
-        .then((res) => {
-          res.data.pipe(fs.createWriteStream(`${folder}${filename}`));
-          success(`Image was saved to ${folder}${filename}`);
-        })
-        .catch((err) => {
-          error(err);
-        });
+      getAvatar(url, filename);
     } else {
       warn('Something went wrong');
     }
@@ -44,10 +49,18 @@ const getAction = (opts) => {
 const makeAction = (opts) => {
   debug(opts);
 
-  log('Make your ava!');
-  success('Done');
+  inquirer.prompt(makeQuestions).then((answers) => {
+    debug(answers);
+    if (answers.id && answers.eyes
+      && answers.nose && answers.mouth) {
+      const filename = `${answers.id}.png`;
+      const url = `${config.API_URL}face/${answers.eyes}/${answers.nose}/${answers.mouth}/${answers.color}`;
+      getAvatar(url, filename);
+    } else {
+      warn('Something went wrong');
+    }
+  });
 };
-
 
 module.exports = {
   getAction,
